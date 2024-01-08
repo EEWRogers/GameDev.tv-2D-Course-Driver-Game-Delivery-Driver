0e003effd0994e0cd85d757bafd31da1e94f3da4
Elliott Rogers <elliott.e.w.rogers@gmail.com>
2024-01-08 14:09:57 +0000

    Added and set up the new Unity input system and created a player movement binding

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -1,9 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Driver : MonoBehaviour
 {
+    [SerializeField] float movementSpeed = 0.01f;
+    [SerializeField] float turnSpeed = 0.1f;
+
+    PlayerInput playerInput;
+    InputAction moveAction;
+
+    void Awake() 
+    {
+        playerInput = GetComponent<PlayerInput>();
+        moveAction = playerInput.actions["Move"];
+    }
+
     void Start()
     {
         
@@ -11,6 +24,7 @@ public class Driver : MonoBehaviour
 
     void Update()
     {
-        transform.Rotate(0,0,10 * Time.deltaTime);
+        transform.Rotate(0,0,turnSpeed);
+        transform.Translate(0,movementSpeed,0);
     }
 }
