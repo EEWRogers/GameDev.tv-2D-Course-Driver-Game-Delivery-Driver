6ae67514fc572879ebcd0773380a34d913bb132e
Elliott Rogers <elliott.e.w.rogers@gmail.com>
2024-01-10 16:18:44 +0000

    Added functionality that changes the player car colour to the colour of the package they are currently carrying

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -7,37 +7,76 @@ public class Delivery : MonoBehaviour
 {
     [SerializeField] PackageType currentPackage;
 
+    SpriteRenderer spriteRenderer;
+    Color defaultColour;
+
+    void Awake() 
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        defaultColour = spriteRenderer.color;
+    }
+
     void OnEnable() 
     {
         currentPackage = PackageType.EMPTY;
     }
 
+    void Update()
+    {
+        SetCarColour();
+    }
+    
     void OnTriggerEnter2D(Collider2D other)
     {
-        GetPackage(other);
-
-        DeliverPackage(other);
-    }
-
-    void GetPackage(Collider2D other)
-    {
-        if (other.GetComponent<PackagePickup>() == null) { return; }
-
-        if (currentPackage == PackageType.EMPTY)
         {
-            currentPackage = other.GetComponent<PackagePickup>().PackageType;
-            other.GetComponent<PackagePickup>().DestroyPackage();
+            GetPackage(other);
+
+            DeliverPackage(other);
+        }
+
+        void GetPackage(Collider2D other)
+        {
+            if (other.GetComponent<PackagePickup>() == null) { return; }
+
+            if (currentPackage == PackageType.EMPTY)
+            {
+                currentPackage = other.GetComponent<PackagePickup>().PackageType;
+                other.GetComponent<PackagePickup>().DestroyPackage();
+            }
+        }
+
+        void DeliverPackage(Collider2D other)
+        {
+            if (other.GetComponent<DeliveryZone>() == null) { return; }
+
+            if (currentPackage == other.GetComponent<DeliveryZone>().PackageType)
+            {
+                Debug.Log("Package delivered!");
+                currentPackage = PackageType.EMPTY;
+            }
         }
     }
 
-    void DeliverPackage(Collider2D other)
+    void SetCarColour()
     {
-        if (other.GetComponent<DeliveryZone>() == null) { return; }
-
-        if (currentPackage == other.GetComponent<DeliveryZone>().PackageType)
+        switch (currentPackage)
         {
-            Debug.Log("Package delivered!");
-            currentPackage = PackageType.EMPTY;
+            case PackageType.RED:
+                spriteRenderer.color = Color.red;
+                break;
+
+
+            case PackageType.GREEN:
+                spriteRenderer.color = Color.green;
+                break;
+
+            case PackageType.BLUE:
+                spriteRenderer.color = Color.blue;
+                break;
+
+            default:
+                spriteRenderer.color = defaultColour;
+                break;
         }
     }
 }
